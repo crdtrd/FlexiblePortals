b984b94c844189b8434d327733a7667988875d2a
crdtrd <crdtrd@gmail.com>
2025-09-17 07:25:57 -0700

    Yuh

--- src/main/java/com/drtdrc/mineableendportalframe/MineableEndPortalFrame.java
+++ src/main/java/com/drtdrc/mineableendportalframe/MineableEndPortalFrame.java
@@ -2,7 +2,7 @@ package com.drtdrc.mineableendportalframe;
 
 import net.fabricmc.api.ModInitializer;
 
-public class Mineableendportalframe implements ModInitializer {
+public class MineableEndPortalFrame implements ModInitializer {
 
     @Override
     public void onInitialize() {
