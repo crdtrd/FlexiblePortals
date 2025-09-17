9c8e7bb050a38f1ad126a7a003f845a7c89aa9de
crdtrd <crdtrd@gmail.com>
2025-09-17 07:25:17 -0700

    Initial commit

---
+++ src/main/java/com/drtdrc/mineableendportalframe/MineableEndPortalFrame.java
@@ -0,0 +1,10 @@
+package com.drtdrc.mineableendportalframe;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mineableendportalframe implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
